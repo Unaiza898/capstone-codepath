2e1b28c5ed10fa53bc6fa3c1e4b2d587b20369c4
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2023-10-20 20:04:48 -0400

    Step 7: Updating a course

--- server/controllers/courses.js
+++ server/controllers/courses.js
@@ -45,3 +45,21 @@ const getCourse = async (req, res) => {
     }
 }
 
+
+// update the course
+
+const updateCourse = async (req, res) => {
+
+    try{
+        const{title, instructor, duration, description, rating, reviews, start_date, end_date, cost} = req.body
+        const id = parseInt(req.params.id)
+
+        const results = await pool.query('UPDATE courses SET title = $1, instructor = $2, duration = $3, description = $4, rating = $5, reviews = $6, start_date = $7, end_date = $8, cost = $9 WHERE id = $10 RETURNING *', [title, instructor, duration, description, rating, reviews, start_date, end_date, cost, id])
+
+        res.status(200).json(results.rows[0])
+    }
+    catch(error){
+        res.status(409).json( { error: error.message } )
+      }
+}
+
