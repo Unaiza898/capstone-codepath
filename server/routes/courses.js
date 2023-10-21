c4c1876ac4959287695d57c844698822c6f00f78
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2023-10-20 20:13:49 -0400

    Step 10: adding routes

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -1,11 +1,18 @@
 import express from 'express'
-
+import CourseController from '../controllers/CourseController.js'
 
 const router = express.Router()
 
 
 
 // @route   GET api/courses
+
+
+router.get('/', CourseController.getCourses)
+router.get('/:id', CourseController.getCourseById)
+router.post('/', CourseController.createCourse)
+router.delete('/:id', CourseController.deleteCourse)
+router.patch('/:id', CourseController.updateCourse)
 
 
 export default router
