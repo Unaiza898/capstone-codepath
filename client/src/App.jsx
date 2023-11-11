d08d037cd056837333c746aab0b652e5fac21ba5
Nikko D <ndasalla1323@gmail.com>
2023-11-11 02:18:01 -0500

    added Login, Register, AuthContext and Homepage pages and also added Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Root from "./pages/Root";
+import Homepage from "./pages/Homepage";
+import Register from "./pages/authentication/Register";
+import Login from "./pages/authentication/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children: [],
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
   },
 ]);
 
