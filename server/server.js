45172db42ca8cd626cdb1b372a961e6e8a998350
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2023-11-17 16:18:59 -0500

    change server.js

--- server/server.js
+++ server/server.js
@@ -1,77 +1,22 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import cors from 'cors'
 
-import courseRoutes from "./routes/courses.js";
-
-import passport from "passport";
-import session from "express-session";
-import { GitHub } from "./config/auth.js";
-import authRoutes from "./routes/auth.js";
+import courseRoutes from './routes/courses.js'
 // create express app
-const app = express();
+const app = express()
 
-app.use(
-  session({
-    secret: "sq7taigbtwo2brby",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+app.use(express.json())
+app.use(cors())
 
-const CLIENT_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://fredngo-cp-w103-lab9-client.up.railway.app"
-    : "http://localhost:3000";
+app.get('/', (req, res) => {
+    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ Capstone project </h1>')
+})
 
-app.use(express.json());
-app.use(
-  cors({
-    origin: CLIENT_URL,
-    methods: "GET,POST,PUT,DELETE,PATCH",
-    credentials: true,
-  })
-);
+app.use('/api/courses', courseRoutes)
 
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(GitHub);
 
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
-
-app.get("/", (req, res) => {
-  res.redirect(CLIENT_URL);
-});
-
-// authentication routes
-app.use("/auth", authRoutes);
-
-// app routes
-app.use("/api/courses", courseRoutes);
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
-
-// app.get("/", (req, res) => {
-//   res
-//     .status(200)
-//     .send(
-//       '<h1 style="text-align: center; margin-top: 50px;">✈️ Capstone project </h1>'
-//     );
-// });
-
-// app.use("/api/courses", courseRoutes);
-
-// const PORT = process.env.PORT || 3001;
-
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on http://localhost:${PORT}`);
-// });
+})
