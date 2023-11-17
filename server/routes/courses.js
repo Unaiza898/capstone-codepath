0077ba91029ea76f72cf731ec4fbca55e520eddb
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2023-11-17 16:14:19 -0500

    updt

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -1,13 +1,10 @@
 import express from 'express'
-import CourseController from '../controllers/courses.js'
-import express from "express";
+
 import CourseController from "../controllers/courses.js";
 
 const router = express.Router();
 
 // @route   GET api/courses
-
-router.get("/", CourseController.getCourses);
 router.get("/:id", CourseController.getCourse);
 router.post("/", CourseController.createCourse);
 router.delete("/:id", CourseController.deleteCourse);
--- server/server.js
+++ server/server.js
@@ -1,77 +1,51 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import cors from 'cors'
 
-import courseRoutes from "./routes/courses.js";
-
-import passport from "passport";
-import session from "express-session";
-import { GitHub } from "./config/auth.js";
-import authRoutes from "./routes/auth.js";
+import courseRoutes from './routes/courses.js'
 // create express app
-const app = express();
 
-app.use(
-  session({
-    secret: "sq7taigbtwo2brby",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+import passport from 'passport'
+import session from 'express-session'
+import { GitHub } from './config/auth.js'
+import authRoutes from './routes/auth.js'
 
-const CLIENT_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://fredngo-cp-w103-lab9-client.up.railway.app"
-    : "http://localhost:3000";
+const app = express()
+app.use(session({
+  secret: 'codepath', // put in .env!!!!!!
+  resave: false,
+  saveUninitialized: true
+}))
 
-app.use(express.json());
-app.use(
-  cors({
-    origin: CLIENT_URL,
-    methods: "GET,POST,PUT,DELETE,PATCH",
-    credentials: true,
-  })
-);
+app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: 'GET,POST,PUT,DELETE,PATCH',
+  credentials: true
+}))
 
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(GitHub);
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(GitHub)
 
 passport.serializeUser((user, done) => {
-  done(null, user);
-});
+  done(null, user)
+})
 
 passport.deserializeUser((user, done) => {
-  done(null, user);
-});
+  done(null, user)
+})
 
-app.get("/", (req, res) => {
-  res.redirect(CLIENT_URL);
-});
-
+app.get('/', (req, res) => {
+  res.redirect('http://localhost:3000')
+})
 // authentication routes
-app.use("/auth", authRoutes);
+app.use('/auth', authRoutes)
 
-// app routes
-app.use("/api/courses", courseRoutes);
+app.use('/api/courses', courseRoutes)
 
-const PORT = process.env.PORT || 3001;
+
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
-
-// app.get("/", (req, res) => {
-//   res
-//     .status(200)
-//     .send(
-//       '<h1 style="text-align: center; margin-top: 50px;">✈️ Capstone project </h1>'
-//     );
-// });
-
-// app.use("/api/courses", courseRoutes);
-
-// const PORT = process.env.PORT || 3001;
-
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on http://localhost:${PORT}`);
-// });
+})
