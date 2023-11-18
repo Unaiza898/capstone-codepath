0d9520cc9d0bc2f2c2c3b7f0db0f186b9cce98b8
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2023-11-17 19:30:02 -0500

    commit

--- server/server.js
+++ server/server.js
@@ -3,14 +3,43 @@ import cors from 'cors'
 
 import courseRoutes from './routes/courses.js'
 // create express app
+
+import passport from 'passport'
+import session from 'express-session'
+import { GitHub } from './config/auth.js'
+import authRoutes from './routes/auth.js'
+
 const app = express()
+app.use(session({
+  secret: 'codepath', // put in .env!!!!!!
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: 'GET,POST,PUT,DELETE,PATCH',
+  credentials: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(GitHub)
+
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+  done(null, user)
+})
 
 app.get('/', (req, res) => {
-    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ Capstone project </h1>')
+  res.redirect('http://localhost:3000')
 })
+// authentication routes
+app.use('/auth', authRoutes)
 
 app.use('/api/courses', courseRoutes)
 
